b2b6fa73735e330888732593c152db5363214020
Nejc Šneberger <nejc.snebi@gmail.com>
2023-04-02 12:25:21 +0200

    Commited ST changes

--- ST/dev/Homework1/script.js
+++ ST/dev/Homework1/script.js
@@ -13,6 +13,11 @@ let backModal = document.getElementById("back-modal");
 backModal.addEventListener("click", () => {
 	let modal = document.getElementById("cards-modal");
 	hideModal(modal);
+	modal.childNodes.forEach((node) => {
+		if (node.nodeName == "DIV") {
+			node.remove();
+		}
+	});
 });
 
 window.onload = () => {
@@ -45,6 +50,10 @@ function setup() {
 	let addCategoryForm = document.getElementById("add-form");
 	addCategoryForm.addEventListener("submit", (e) => {
 		let category = document.getElementById("category-name").value;
+		if (category == "") {
+			e.preventDefault();
+			return;
+		}
 		inventory.push({ [category]: [] });
 		localStorage.setItem("inventory", JSON.stringify(inventory));
 		hideModal();
@@ -55,17 +64,17 @@ function generateCards(n) {
 	let cards = document.getElementById("cards");
 	if (inventory.length > 0) {
 		if (inventory.length < n) {
-			inventory.forEach((card) => {
+			inventory.forEach((card, i) => {
 				let text = Object.keys(card).toString();
 				let length = Object.values(card)[0].length;
-				let div = generateCard(text, length);
+				let div = generateCard(text, length, i);
 				cards.appendChild(div);
 			});
 		} else {
 			for (let i = 0; i < n - 1; i++) {
 				let text = Object.keys(inventory[i]).toString();
 				let length = Object.values(inventory[i])[0].length;
-				let div = generateCard(text, length);
+				let div = generateCard(text, length, i);
 				cards.appendChild(div);
 			}
 			let div = generateMoreCard();
@@ -77,7 +86,7 @@ function generateCards(n) {
 	cards.appendChild(div);
 }
 
-function generateCard(txt, len) {
+function generateCard(txt, len, i) {
 	let text = document.createTextNode(txt);
 	let small = document.createElement("small");
 	small.appendChild(document.createTextNode(len + " cards"));
@@ -86,8 +95,17 @@ function generateCard(txt, len) {
 	div.appendChild(text);
 	div.appendChild(small);
 	div.addEventListener("click", (e) => {
-		let cardModal = document.getElementById("cards-modal");
-		cards = showModal(cardModal);
+		let cardsModal = document.getElementById("cards-modal");
+		showModal(cardsModal);
+		// Object.values(inventory[i]).forEach((card) => {
+		// 	card.forEach((question) => {
+		// 		let q = Object.keys(question).toString();
+		// 		Object.values(question)[0].forEach((answer) => {
+		// 			let a = Object.keys(answer).toString();
+		// 		});
+		// 	});
+		// });
+		generateCategoryCards(i, cardsModal);
 	});
 	return div;
 }
@@ -115,6 +133,25 @@ function generateAddCard() {
 		showModal(modal);
 	});
 	return div;
+}
+
+function generateCategoryCards(i, modal) {
+	Object.values(inventory[i]).forEach((card) => {
+		card.forEach((question) => {
+			let div = document.createElement("div");
+			let text = Object.keys(question).toString();
+			let q = document.createTextNode(text);
+			div.appendChild(q);
+			Object.values(question)[0].forEach((answer) => {
+				let a = Object.keys(answer).toString();
+				let small = document.createElement("small");
+				small.appendChild(document.createTextNode(a));
+				div.classList.add("grid-item");
+				div.appendChild(small);
+			});
+			modal.appendChild(div);
+		});
+	});
 }
 
 function showModal(modal) {
