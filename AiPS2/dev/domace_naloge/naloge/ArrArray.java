e741e6d295c22d99b48357f5263cac55560ca345
Nejc Šneberger <nejc.snebi@gmail.com>
2023-04-18 18:01:55 +0200

    AiPS2 work

--- AiPS2/dev/domace_naloge/naloge/ArrArray.java
+++ AiPS2/dev/domace_naloge/naloge/ArrArray.java
@@ -1,17 +1,16 @@
 package naloge;
 
-import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ArrArray {
     // null ... prazen element, -1 ... leno brisan element
-
-    private static Integer[][] array;
-    private static int[] occurenceCounter;
+    private static List<ArrayElement[]> array;
     private static int size;
     private static int[] bloomFilter;
 
     ArrArray() {
-        array = new Integer[2][2];
+        array = new ArrayList<>();
         size = 0;
         bloomFilter = new int[10];
     }
@@ -38,17 +37,20 @@ public class ArrArray {
 
         int k = (int) Math.floor(Math.log(size) / Math.log(2) + 1);
         for (int i = 0; i < k; i++) {
+            // if A_i is empty continue
+
+
             int low = 0;
             int high = (int) Math.pow(2, i) - 1;
 
-            if (array[i][low] > e || array[i][high] < e) continue;
+            if (array.get(i)[low].compareTo(e) == 1 || array.get(i)[high].compareTo(e) == -1) continue;
 
             while (low <= high) {
                 int j = low + ((high - low) / 2);
-                if (array[i][j] == e) return true;
-                if (array[i][j] < e) {
+                if (array.get(i)[j].compareTo(e) == 0) return true;
+                if (array.get(i)[j].compareTo(e) == -1) {
                     low = j + 1;
-                } else if (array[i][j] > e) {
+                } else if (array.get(i)[j].value > e) {
                     high = j - 1;
                 }
             }
@@ -59,8 +61,8 @@ public class ArrArray {
     public boolean delete(int e) {
         if (!elementInArray(e)) return false;
 
-        if (array[0][0] == e) {
-            array[0][0] = -1;
+        if (array.get(0)[0].compareTo(e) == 0) {
+            array.get(0)[0].setValue(-1);
             return true;
         }
 
@@ -70,22 +72,22 @@ public class ArrArray {
             int low = 0;
             int high = (int) Math.pow(2, i) - 1;
 
-            if (array[i][low] > e || array[i][high] < e) continue;
+            if (array.get(i)[low].compareTo(e) == 1 || array.get(i)[low].compareTo(e) == -1) continue;
 
             while (low <= high) {
                 int j = low + ((high - low) / 2);
-                if (array[i][j] == e) {
-                    if (duplicates[e] > 0) {
+                if (array.get(i)[j].compareTo(e) == 0) {
+                    /*if (duplicates[e] > 0) {
                         duplicates[e]--;
                     } else {
-                        array[i][j] = -1;
-                    }
+                        array.get(i)[j].value = -1;
+                    }*/
                     size--;
                     return true;
                 }
-                if (array[i][j] < e) {
+                if (array.get(i)[j].compareTo(e) == -1) {
                     low = j + 1;
-                } else if (array[i][j] > e) {
+                } else if (array.get(i)[j].compareTo(e) == 1) {
                     high = j - 1;
                 }
             }
@@ -102,17 +104,16 @@ public class ArrArray {
         int k = (int) Math.floor(Math.log(size) / Math.log(2) + 1);
         for (int i = 0; i < k; i++) {
             System.out.printf("%s%d:", "A_", i);
-            int levelSize = (int) Math.pow(2, i);
-            if ((size & levelSize) == levelSize) {
-                if (array[i][0] != null) {
-                    System.out.printf("%d/%d", array[i][0], duplicates[array[i][0]]);
-                } else if (array[i][0] == -1) {
+            if (!isEmpty(i)) {
+                if (array.get(i)[0].getValue() != null) {
+                    System.out.printf("%d/%d", array.get(i)[0].getValue(), array.get(i)[0].getCounter());
+                } else if (array.get(i)[0].isDeleted) {
                     System.out.printf("x");
                 }
-                for (int j = 1; j < levelSize; j++) {
-                    if (array[i][j] != null) {
-                        System.out.printf(", %d/%d", array[i][j], duplicates[array[i][j]]);
-                    } else if (array[i][j] == -1) {
+                for (int j = 1; j < (int) Math.pow(2, i); j++) {
+                    if (array.get(i)[j].getValue() != null) {
+                        System.out.printf(", %d/%d", array.get(i)[j].getValue(), array.get(i).getCounter());
+                    } else if (array.get(i)[j].compareTo(-1) == 0) {
                         System.out.printf(", x");
                     }
                 }
@@ -123,6 +124,10 @@ public class ArrArray {
         }
     }
 
+    private boolean isEmpty(int level) {
+        int levelSize = (int) Math.pow(2, level);
+        return (size & levelSize) == 0;
+    }
 
     private void hash(int e) {
         while (e > 1) {
@@ -141,7 +146,6 @@ public class ArrArray {
         return true;
     }
 
-
     private static void resize() {
 
     }
@@ -150,4 +154,40 @@ public class ArrArray {
 
     }
 
+    class ArrayElement implements Comparable<Integer> {
+        private Integer value;
+        int duplicateCounter;
+        boolean isDeleted;
+
+        ArrayElement() {
+            duplicateCounter = 0;
+            isDeleted = false;
+        }
+
+        @Override
+        public int compareTo(Integer x) {
+            return this.value > x ? 1 : this.value == x ? 0 : -1;
+        }
+
+        public void setValue(Integer value) {
+            this.value = value;
+        }
+
+        public Integer getValue() {
+            return value;
+        }
+
+        public void setDuplicateCounter(int x) {
+            duplicateCounter += x;
+        }
+
+        public int getDuplicateCounter() {
+            return duplicateCounter;
+        }
+
+        public void lazyDelete() {
+            isDeleted = true;
+            duplicateCounter--;
+        }
+    }
 }
