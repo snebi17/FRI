d6588cf55618a15efdd456a46a4a17cde53bd1db
Nejc Šneberger <nejc.snebi@gmail.com>
2023-05-19 00:55:38 +0200

    UV

--- UV/LabWork5/app/src/main/java/com/example/labwork5/Checkout.java
+++ UV/LabWork5/app/src/main/java/com/example/labwork5/Checkout.java
@@ -1,14 +1,49 @@
 package com.example.labwork5;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
+import android.media.MediaCodec;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.Calendar;
+import java.util.List;
 
 public class Checkout extends AppCompatActivity {
-
+    private List<Details.Passenger> passengers;
+    private LinearLayout content;
+    private int total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_checkout);
+
+        Intent i = getIntent();
+        passengers = (List<Details.Passenger>) i.getSerializableExtra("PASSENGERS");
+
+        content = findViewById(R.id.content);
+        passengers.forEach(passenger -> {
+            TextView textView = new TextView(this);
+            textView.setText(passenger.fullName);
+            content.addView(textView);
+
+            Calendar c = Calendar.getInstance();
+            c.setTimeInMillis(passenger.age);
+            int age = c.get(Calendar.YEAR);
+
+            if (age > 2 && age <= 12) {
+                total += passenger.returnTicket ? 75 : 50;
+            } else if (age > 12) {
+                total += passenger.returnTicket ? 150 : 100;
+            }
+        });
     }
+
+
 }
--- UV/LabWork5/app/src/main/java/com/example/labwork5/Details.java
+++ UV/LabWork5/app/src/main/java/com/example/labwork5/Details.java
@@ -4,20 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
 import java.util.List;
 
 public class Details extends AppCompatActivity {
     List<Passenger> passengers;
-    Passenger passenger;
+    TextView errors;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        passengers = new ArrayList<>();
 
         String city = getIntent().getStringExtra("CITY");
         int image = getIntent().getIntExtra("IMAGE", 0);
@@ -27,29 +36,52 @@ public class Details extends AppCompatActivity {
 
         tvCity.setText(city);
         imageView.setImageResource(image);
+
+        errors = findViewById(R.id.errors);
     }
 
-    public void onAddPerson(String fullName, int age) {
-        passengers.add(passenger);
+    public void onAddPerson(View v) {
+        TextView textView = (TextView) findViewById(R.id.fullName);
+        DatePicker datePicker = (DatePicker) findViewById(R.id.datePicker);
+        if (!textView.getText().toString().equals("") && datePicker != null) {
+            errors.setText("");
+            String fullName = textView.getText().toString();
+            int day = datePicker.getDayOfMonth();
+            int month = datePicker.getMonth() + 1;
+            int year = datePicker.getYear();
+            Date date = new GregorianCalendar(year, month - 1, day).getTime();
+            Date currDate = new Date();
+            long diff = currDate.getTime() - date.getTime();
+            CheckBox checkBox = findViewById(R.id.returnTicket);
+            boolean returnTicket = checkBox.isChecked();
+            passengers.add(new Passenger(fullName, diff, returnTicket));
+            textView.setText("");
+            checkBox.clearFocus();
+        } else {
+            errors.setText("Izpolnite vsa polja!");
+        }
     }
 
-    public void proceedToCheckout() {
-        Intent intent = new Intent(this, Checkout.class);
-        intent.putExtra("PASSENGERS", new Serializable() {
-            @Override
-            public int hashCode() {
-                return super.hashCode();
-            }
-        });
+    public void proceedToCheckout(View v) {
+        if (passengers.size() > 0) {
+            errors.setText("");
+            Intent intent = new Intent(Details.this, Checkout.class);
+            intent.putExtra("PASSENGERS", (Serializable) passengers);
+            startActivity(intent);
+        } else {
+            errors.setText("Dodajte vsaj enega potnika.");
+        }
     }
 
-    class Passenger {
+    public static class Passenger implements Serializable {
         String fullName;
-        int age;
+        long age;
+        boolean returnTicket;
 
-        Passenger(String fullName, int age) {
+        Passenger(String fullName, long age, boolean returnTicket) {
             this.fullName = fullName;
             this.age = age;
+            this.returnTicket = returnTicket;
         }
     }
 }
